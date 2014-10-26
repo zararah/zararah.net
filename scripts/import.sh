38ca32b09ec45077ca71ed99508ada20ed4747de
Zara Rahman <zara.rahman@okfn.org>
2014-10-26 17:28:19 +0100

    Change import script to html to get images

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,7 +1,7 @@
 ruby -rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::Tumblr.run({
       "url"            => "http://z-rah.tumblr.com",
-      "format"         => "md", # or "md"
+      "format"         => "html", # or "md"
       "grab_images"    => true,  # whether to download images as well.
       "add_highlights" => true,  # whether to wrap code blocks (indented 4 spaces) in a Liquid "highlight" tag
       "rewrite_urls"   => false   # whether to write pages that redirect from the old Tumblr paths to the new Jekyll paths
